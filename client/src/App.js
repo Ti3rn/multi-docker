435605f44a3a099e9132d0ef95140edec42e2b63
tiern <create.by.ti3n@gmail.com>
2019-09-11 17:47:48 +0700

    change h1 on index.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Fib Calculator</h1>
           <Link to="/">HOME</Link>
           <Link to="/otherpage">OTHERPAGE</Link>
         </header>
